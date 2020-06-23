5fde93b96b1dc0ddca8450c029ca349319e37010
Amar Gupta <amar9911056196@gmail.com>
2020-06-23 17:20:17 +0530

    update

--- src/Pages/Projects/ProjectData2.js
+++ src/Pages/Projects/ProjectData2.js
@@ -1,60 +1,70 @@
 const ProjectData2 = [
-    {
-      id: 1,
-      title: "Ecommerce App",
-      imageUrl: "https://i.ibb.co/VVDtKVb/3.jpg",
-      description: `A massive Ecommerce app made with React, Redux, Hooks, GraphQL, ContextAPI, Stripe, Firebase and covers a lot of 
+  {
+    id: 1,
+    title: 'Ecommerce App',
+    imageUrl: 'https://i.ibb.co/VVDtKVb/3.jpg',
+    description: `A massive Ecommerce app made with React, Redux, Hooks, GraphQL, ContextAPI, Stripe, Firebase and covers a lot of 
       advanced concepts of frontend technologies such as Redux-Sagas, React Router, Asynchronous Redux, Styled-Components, 
       React Suspense & Lazy, State Normalization, Design Patterns, App Architecture and more.`,
-      github: "https://github.com/amar-gupta-au8/crown-cart",
-      live: "https://amar-crown-cart.netlify.app/"
-    },
-    {
-      id: 2,
-      title: "Netflix Clone",
-      imageUrl: "https://i.ibb.co/brcXYzL/netflix-clone.jpg",
-      description: `Netflix clone made with advanced react, redux & firebase which uses redux thunk to asynchronously fetch movie data of over 30 thousand movies from Tmdb api. Features include a multi-active searchbar, user signup and signin, add to list option for each user, trailer links, dynamically generated movie and tv show pages and an interactive UI replicated from the original netflix app. `,
-      github: "/",
-      live: "/"
-    },
-    {
-      id: 3,
-      title: "Asur",
-      imageUrl: "https://i.ibb.co/Qkz3wtT/Screenshot-2020-06-22-at-1-00-16-PM.png",
-      description: `Asur is a React app that lets you search between different API generated robots with name with a custom made search bar
+    github: 'https://github.com/amar-gupta-au8/crown-cart',
+    live: 'https://amar-crown-cart.netlify.app/',
+  },
+  {
+    id: 2,
+    title: 'Netflix Clone',
+    imageUrl: 'https://i.ibb.co/brcXYzL/netflix-clone.jpg',
+    description: `Netflix clone made with advanced react, redux & firebase which uses redux thunk to asynchronously fetch movie data of over 30 thousand movies from Tmdb api. Features include a multi-active searchbar, user signup and signin, add to list option for each user, trailer links, dynamically generated movie and tv show pages and an interactive UI replicated from the original netflix app. `,
+    github: '/',
+    live: '/',
+  },
+  {
+    id: 3,
+    title: 'Asur',
+    imageUrl:
+      'https://i.ibb.co/Qkz3wtT/Screenshot-2020-06-22-at-1-00-16-PM.png',
+    description: `Asur is a React app that lets you search between different API generated robots with name with a custom made search bar
       by using the power of robohash api. This is a perfect project for introduction to the world of React and API's. It's fully responsive and functional in 
       different screen sizes.`,
-      github: "https://github.com/amar-gupta-au8/asur-react",
-      live: "https://amar-asur.netlify.app/"
-    },
-    {
-      id: 4,
-      title: "Animated Blog",
-      imageUrl: "https://i.ibb.co/jwR0kw3/3.png",
-      description: `A project based my learning on I started learning full stack web Developer about a month ago and wanted to write on the ups and downs of my first full-time development role.
+    github: 'https://github.com/amar-gupta-au8/asur-react',
+    live: 'https://amar-asur.netlify.app/',
+  },
+  {
+    id: 4,
+    title: 'Animated Blog',
+    imageUrl: 'https://i.ibb.co/jwR0kw3/3.png',
+    description: `A project based my learning on I started learning full stack web Developer about a month ago and wanted to write on the ups and downs of my first full-time development role.
       Day 1 is a bit of a blur looking back. Prior to this role, I had only learned tiny and small ones at that. Now I found myself a small fish in a much bigger pond than I was used to`,
-      github: "https://github.com/amar-gupta-au8/react-first-blogpost.github.io",
-      live: "https://amar-gupta-au8.github.io/react-first-blogpost.github.io/"
-    },
-    {
-      id: 6,
-      title: "Portfolio",
-      imageUrl: "https://i.ibb.co/X4kLNYp/3.png",
-      description: `Portfolio – A Successful Dropshipping Ecommerce Store with 390+ products in inventory customized with a theme and high 
+    github: 'https://github.com/amar-gupta-au8/react-first-blogpost.github.io',
+    live: 'https://amar-gupta-au8.github.io/react-first-blogpost.github.io/',
+  },
+  {
+    id: 5,
+    title: 'Portfolio',
+    imageUrl: 'https://i.ibb.co/X4kLNYp/3.png',
+    description: `Portfolio – A Successful Dropshipping Ecommerce Store with 390+ products in inventory customized with a theme and high 
       emphasis on its SEO and social media marketing and a smooth and responsive UI using wordpress with various plugins. 
       Features include chat bot for visitors, bulk compression of images, login with facebook integration.`,
-      github: "https://github.com/amar-gupta-au8/profile5",
-      live: "https://amar-profile5.netlify.app/#/"
-    },
-   
-  ];
-  
-  export default ProjectData2;
-  
-  // https://i.ibb.co/JdXZJG8/1.jpg
-  // https://i.ibb.co/DRmbpn9/2.jpg
-  // https://i.ibb.co/VVDtKVb/3.jpg
-  // https://i.ibb.co/6bTFrzC/4.jpg
-  // https://i.ibb.co/mtgK29T/5.jpg
-  // https://i.ibb.co/PgfJdWv/6.jpg
-  
+    github: 'https://github.com/amar-gupta-au8/profile5',
+    live: 'https://amar-profile5.netlify.app/#/',
+  },
+  {
+    id: 6,
+    title: 'Awosome React Blog',
+    imageUrl:
+      'https://i.ibb.co/hCLQ9mf/Screenshot-2020-06-23-at-5-18-15-PM.png',
+    description: `Portfolio – A Successful Dropshipping Ecommerce Store with 390+ products in inventory customized with a theme and high 
+      emphasis on its SEO and social media marketing and a smooth and responsive UI using wordpress with various plugins. 
+      Features include chat bot for visitors, bulk compression of images, login with facebook integration.`,
+    github: 'https://github.com/amar-gupta-au8/Awosome-React-blog',
+    live: 'https://awosome-blog.netlify.app/home',
+  },
+];
+
+export default ProjectData2;
+
+// https://i.ibb.co/JdXZJG8/1.jpg
+// https://i.ibb.co/DRmbpn9/2.jpg
+// https://i.ibb.co/VVDtKVb/3.jpg
+// https://i.ibb.co/6bTFrzC/4.jpg
+// https://i.ibb.co/mtgK29T/5.jpg
+// https://i.ibb.co/PgfJdWv/6.jpg
