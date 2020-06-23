2bff78fc129449fcd0f76104e3c5cc2d01fb3700
Amar Gupta <amar9911056196@gmail.com>
2020-06-23 17:11:39 +0530

    update

--- src/Pages/Projects/ProjectData.js
+++ src/Pages/Projects/ProjectData.js
@@ -30,6 +30,16 @@ const ProjectData = [
     github: "/",
     live: "/"
   },
+  {
+    id: 4,
+    title: "The Web Dev",
+    imageUrl: "https://i.ibb.co/G9BkwKH/Screenshot-2020-06-23-at-5-07-21-PM.png",
+    description: `A project based on implementing the most advanced concepts and features of CSS by using SASS, BEM (Block Element Modifier),
+	 Transitions, Color-gradients, Background Images, Background Videos and more. It contains dozens of animations and design features and 
+	 almost everything one can do with the power of CSS.`,
+    github: "https://github.com/amar-gupta-au8/thewebdev",
+    live: "https://thewebdev.herokuapp.com/"
+  },
   
   
  
